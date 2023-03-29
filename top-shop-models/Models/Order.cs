ec8d7cca99809d5472c0abdd179eef3487eee8c2
Alexander Maskov <onetwothree_1@mail.ru>
2023-03-29 21:15:30 +0300

    Finished all model files. Prep to db_connect

---
+++ top-shop-models/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace top_shop_models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public double Price { get; set; }
+        public virtual Client Client { get; set; }
+
+        public virtual ICollection<ItemOrder> ItemOrders { get; set; }
+
+    }
+}
